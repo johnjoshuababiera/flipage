f471ea8b15e2a8b3effc5480b33d2d13975e71ff
Joshua <johnjoshuababiera@yahoo.com>
2018-05-02 10:24:35 +0800

    main app

---
+++ resource/src/main/java/com/cpu/FlipageApplication.java
@@ -0,0 +1,12 @@
+package com.cpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlipageApplication.class, args);
+    }
+
+}
