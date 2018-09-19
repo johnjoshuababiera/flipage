496e37b71c3b2019e15d53fd0932936c6ec8e76c
Joshua <johnjoshuababiera@yahoo.com>
2018-09-19 21:02:03 +0800

    update find by department id query

--- repository/src/main/java/com/cpu/news/NewsRepository.java
+++ repository/src/main/java/com/cpu/news/NewsRepository.java
@@ -1,11 +1,14 @@
 package com.cpu.news;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface NewsRepository extends JpaRepository<News,Long> {
 
-    List<News> findByDepartmentId(long id);
+    @Query(value = "select n from News n where n.department.id=:id")
+    List<News> findByDepartmentId(@Param("id") long id);
     List<News> findByUserId(long id);
 }
